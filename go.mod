3327eec56a00c82cb82a79e915c7ada9b5084127
Riyaz Ali <me@riyazali.net>
2020-10-23 22:27:25 +0530

    feat: implement initial version of the cli

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/imdario/mergo v0.3.11 // indirect
 	github.com/mattn/go-sqlite3 v1.14.4
+	github.com/pkg/errors v0.8.1
 	go.riyazali.net/sqlite v0.0.0-20201022044758-b7de9cc2f468
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 	golang.org/x/net v0.0.0-20201020065357-d65d470038a5 // indirect
--- main.go
+++ main.go
@@ -20,10 +20,104 @@ package main
 // }
 import "C"
 import (
+	"bufio"
+	"context"
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
 	_ "go.riyazali.net/gitql/gitql"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+	var err error
+	var repo string
 
+	flag.StringVar(&repo, "repo", ".", "path to git repository")
+	flag.Parse()
+
+	if repo, err = filepath.Abs(repo); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to resolve repository path"))
+	}
+
+	var query string
+	if query = flag.Arg(0); query == "" {
+		if fi, err := os.Stdin.Stat(); err == nil {
+			if fi.Mode()&os.ModeCharDevice == 0 {
+				var b []byte
+				if b, err = ioutil.ReadAll(bufio.NewReader(os.Stdin)); err == nil {
+					query = string(b)
+					goto _continue
+				}
+			}
+		}
+		log.Fatal("please provide a valid sql query")
+	}
+
+_continue:
+
+	var db *sql.DB
+	if db, err = sql.Open("sqlite3", fmt.Sprintf("file:%x?mode=memory&cache=shared", repo)); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to open database connection"))
+	}
+	defer db.Close()
+
+	// initialize the virtual tables
+	for _, q := range []string{
+		"CREATE VIRTUAL TABLE commits USING git_log(%q)",
+		"CREATE VIRTUAL TABLE refs    USING git_ref(%q)",
+	} {
+		if _, err = db.Exec(fmt.Sprintf(q, repo)); err != nil {
+			log.Fatal(errors.Wrap(err, "failed to create virtual table"))
+		}
+	}
+
+	var rows *sql.Rows
+	if rows, err = db.QueryContext(context.Background(), query); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to execute query"))
+	}
+	defer rows.Close()
+
+	var columns []*sql.ColumnType
+	if columns, err = rows.ColumnTypes(); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to fetch column details"))
+	}
+
+	var values = make([]interface{}, len(columns))
+	for i := range values {
+		values[i] = new(interface{})
+	}
+
+	var out = json.NewEncoder(os.Stdout)
+	out.SetIndent("", "  ")
+
+	for rows.Next() {
+		if err = rows.Scan(values...); err != nil {
+			log.Fatal(errors.Wrap(err, "failed to scan row"))
+		}
+
+		var dest = make(map[string]interface{})
+		for i, column := range columns {
+			dest[column.Name()] = *(values[i].(*interface{}))
+		}
+
+		if err = out.Encode(dest); err != nil {
+			log.Fatal(errors.Wrap(err, "failed to write record"))
+		}
+	}
+
+	if err = rows.Err(); err != nil {
+		log.Fatal(errors.Wrap(err, "failed to query git data"))
+	}
+}
+
+func init() {
+	log.SetPrefix("gitql: ")
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmsgprefix)
 }
